e79ca86cee5c49f88772926531bd73bd607b33f4
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-10-16 18:03:34 +0100

    feat(about): add and stylize about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,23 @@
+"use client";
 import styles from './style.module.css';
+import Header from "@/components/header/header";
+import {useRouter} from "next/navigation";
 
 export default function About() {
+    const router = useRouter()
     return (
-        <main className={styles.main}></main>
+        <div className={styles.page}>
+            <div className={styles.header}>
+                <Header></Header>
+            </div>
+            <div className={styles.landingPage}>
+                <div className={`${styles.option} ${styles.fadeIn}`} onClick={() => {router.push('/timeline')}}>
+                    <h1 className={styles.title}>Mon Parcours</h1>
+                </div>
+                <div className={`${styles.option} ${styles.fadeIn}`} onClick={() => {router.push('/resume')}}>
+                    <h1 className={styles.title}>Mon C.V.</h1>
+                </div>
+            </div>
+        </div>
     )
 }
