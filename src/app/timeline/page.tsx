7384e4ca1349499313c2e525d99d7c7414c9ca82
C2dricLeroy <cedric.leroy.psycho@gmail.com>
2024-01-17 15:04:13 +0100

    feat(timeline): add timeline

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,7 +1,37 @@
+import React from 'react';
 import styles from './style.module.css';
+import Header from "@/components/header/header";
 
 export default function Timeline() {
+
+    const events = [
+        { year: "12/2023", description: "Fin du dispositif Incubateur numérique" },
+        { year: "10/2023", description: "Entrée au sein de l'incubateur numérique d'Euratechnologies" },
+        { year: "07/2023", description: "Diplômé Concepteur Développeur d'Applications" },
+        { year: "10/2022", description: "Entrée en formation Concepteur Développeur d'Applications"},
+        { year: "03/2022", description: "Découverte du développement web et complétion du CS50"},
+        { year: "10/2021", description: "Psychologue clinicien pour les groupes Korian et Fondation Partage & Vie" },
+        { year: "09/2022", description: "Obtention du Master PCPA de l'Université de Lille et du titre de psychologue clinicien"},
+        { year: "09/2016", description: "Entrée à l'Université de Lille Licence de Psychologie"},
+        { year: "09/2015", description: "1ère année de Médecine P.A.C.E.S."},
+        { year: "09/2015", description: "Obtention du baccalauréat Scientifique mention bien"},
+    ];
+
     return (
-        <main className={styles.main}></main>
-    )
+        <main className={styles.main}>
+            <div className={styles.header}>
+                <Header></Header>
+            </div>
+            <h1>Mon Parcours</h1>
+            <div className={styles.timeline}>
+                <div className={styles.verticalLine}> </div>
+                {events.map((event, index) => (
+                    <div key={index} className={index % 2 === 0 ? styles.eventLeft : styles.eventRight}>
+                        <h2 className={styles.year}>{event.year}</h2>
+                        <p>{event.description}</p>
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
 }
