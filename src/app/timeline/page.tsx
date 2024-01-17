539fee2c6324b3371a5a1c92df2685414747caa6
C2dricLeroy <cedric.leroy.psycho@gmail.com>
2024-01-17 16:28:43 +0100

    feat(timeline): add responsivity for mobile version

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -13,11 +13,11 @@ export default function Timeline() {
         { year: "07/2023", description: "Diplômé Concepteur Développeur d'Applications" },
         { year: "10/2022", description: "Entrée en formation Concepteur Développeur d'Applications"},
         { year: "03/2022", description: "Découverte du développement web et complétion du CS50"},
-        { year: "10/2021", description: "Psychologue clinicien pour les groupes Korian et Fondation Partage & Vie" },
+        { year: "10/2021", description: "Psychologue Clinicien pour les groupes Korian et Fondation Partage & Vie" },
         { year: "09/2022", description: "Obtention du Master PCPA de l'Université de Lille et du titre de psychologue clinicien"},
         { year: "09/2016", description: "Entrée à l'Université de Lille Licence de Psychologie"},
         { year: "09/2015", description: "1ère année de Médecine P.A.C.E.S."},
-        { year: "09/2015", description: "Obtention du baccalauréat Scientifique mention bien"},
+        { year: "09/2015", description: "Obtention du Baccalauréat Scientifique mention bien"},
     ];
 
     useEffect(() => {
